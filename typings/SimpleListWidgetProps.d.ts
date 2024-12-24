5d9ec0a9e38ed76034882a393b10dbdc758067d8
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2024-12-24 13:11:38 +0100

    Mendix 10.18

--- typings/SimpleListWidgetProps.d.ts
+++ typings/SimpleListWidgetProps.d.ts
@@ -27,7 +27,8 @@ export interface SimpleListWidgetPreviewProps {
     style: string;
     styleObject?: CSSProperties;
     readOnly: boolean;
-    renderMode?: "design" | "xray" | "structure";
+    renderMode: "design" | "xray" | "structure";
+    translate: (text: string) => string;
     ds: {} | { caption: string } | { type: string } | null;
     content: { widgetCount: number; renderer: ComponentType<{ children: ReactNode; caption?: string }> };
     selectedItemGuidAttr: string;
