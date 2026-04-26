5e4b60c1cffd593f9f191b61f857873ccb9af2d2
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2026-04-26 14:43:10 +0200

    Widget release build

--- src/components/SimpleListItem.tsx
+++ src/components/SimpleListItem.tsx
@@ -1,4 +1,4 @@
-import React, {  ReactElement } from "react";
+import React, { ReactElement } from "react";
 import { EditableValue, ListWidgetValue, ObjectItem } from "mendix";
 
 export interface SimpleListItemProps {
